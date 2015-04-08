1380fddeef9f3af0fc1c0a79d482953d0bfbf12c
kanthkumar <kkd6428@rit.edu>
2015-04-08 16:23:39 -0400

    Name Node Reply Message

---
+++ src/com/dfs/messages/NameNodeReplyMessage.java
@@ -0,0 +1,11 @@
+package com.dfs.messages;
+
+/**
+ * This class represents message format, the NameNode replies
+ * to Client for read and write requests 
+ *
+ */
+
+public class NameNodeReplyMessage {
+
+}
