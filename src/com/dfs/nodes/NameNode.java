200db3c103190d27b61ce24206c86084ac33cda9
ssuman <suman.dna@gmail.com>
2015-04-08 16:26:00 -0400

    Slaves and NameNode modification
    namenode modification

--- src/com/dfs/nodes/NameNode.java
+++ src/com/dfs/nodes/NameNode.java
@@ -1,5 +1,27 @@
 package com.dfs.nodes;
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 public class NameNode {
 
+	List<String> nodeList ;
+	
+	public NameNode() throws IOException{
+		
+		//Read Data node list from slaves
+		nodeList = new ArrayList<>();
+		File file = new File("slaves");
+		BufferedReader stream = new BufferedReader(new FileReader(file));
+		String node=null;
+		while((node=stream.readLine())!=null)
+			nodeList.add(node);
+		stream.close();
+		
+		
+	}
+	
+	
 }
