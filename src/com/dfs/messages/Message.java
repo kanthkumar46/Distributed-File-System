7490802b379e5fbb0738b87c7d5d6d59ce6c84ee
ssuman <suman.dna@gmail.com>
2015-04-16 23:53:41 -0400

    Modification to Message
    Message type

--- src/com/dfs/messages/Message.java
+++ src/com/dfs/messages/Message.java
@@ -1,9 +1,14 @@
 package com.dfs.messages;
 
+import com.dfs.nodes.RequestType;
+
 public class Message {
+	
 	private String ipAddress;
 	private int portNum;
-	private String fileName;
+	private String sourcePath;
+	private String destinationPath;
+	private RequestType requestType;
 	private int replication;
 	
 	public String getIpAddress() {
@@ -22,19 +27,14 @@ public class Message {
 		this.portNum = portNum;
 	}
 
-	public String getMessage() {
-		return fileName;
-	}
 
-	public void setMessage(String fileName) {
-		this.fileName = fileName;
-	}
-
-	public Message(String ipAddress, int portNum, String fileName,int replication){
+	public Message(String ipAddress, int portNum, String sourcePath,String destPath,int replication, RequestType type){
 		this.ipAddress = ipAddress;
 		this.portNum = portNum;
-		this.fileName = fileName;
+		this.setSourcePath(sourcePath);
+		this.setDestinationPath(destPath);
 		this.replication = replication;
+		this.setRequestType(type);
 	}
 
 	public int getReplication() {
@@ -43,5 +43,32 @@ public class Message {
 
 	public void setReplication(int replication) {
 		this.replication = replication;
+	}
+
+
+	public String getSourcePath() {
+		return sourcePath;
+	}
+
+
+	public void setSourcePath(String sourcePath) {
+		this.sourcePath = sourcePath;
+	}
+
+	public String getDestinationPath() {
+		return destinationPath;
+	}
+
+
+	public void setDestinationPath(String destinationPath) {
+		this.destinationPath = destinationPath;
+	}
+
+	public RequestType getRequestType() {
+		return requestType;
+	}
+
+	public void setRequestType(RequestType requestType) {
+		this.requestType = requestType;
 	}
 }
