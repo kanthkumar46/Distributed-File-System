24237692db2c1fa6d29ff3b0b916be7accb1037d
ssuman <suman.dna@gmail.com>
2015-05-06 15:20:03 -0400

    Ack Message

--- src/com/dfs/messages/AckMessage.java
+++ src/com/dfs/messages/AckMessage.java
@@ -4,13 +4,13 @@ import java.io.Serializable;
 
 public class AckMessage implements Serializable{
 	private String blkId;
-	private String destinationPath;
+	
 	private String ipAddress;
 	
 	public String getBlcokId() {
 		return blkId;
 	}
-	public void setBlcokId(String blockId) {
+	public void setBlockId(String blockId) {
 		this.blkId = blockId;
 	}
 	public String getIpAddress() {
@@ -19,11 +19,6 @@ public class AckMessage implements Serializable{
 	public void setIpAddress(String ipAddress) {
 		this.ipAddress = ipAddress;
 	}
-	public String getDestinationPath() {
-		return destinationPath;
-	}
-	public void setDestinationPath(String destinationPath) {
-		this.destinationPath = destinationPath;
-	}
+
 	
 }
