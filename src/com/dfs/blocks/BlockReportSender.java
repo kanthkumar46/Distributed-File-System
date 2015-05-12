68ea9825fded0de40bb7fc9e4a312a34ad6f0756
kanthkumar <kkd6428@rit.edu>
2015-05-12 19:16:12 -0400

    Heart Beat and Block Report Sender

--- src/com/dfs/blocks/BlockReportSender.java
+++ src/com/dfs/blocks/BlockReportSender.java
@@ -1,5 +1,54 @@
 package com.dfs.blocks;
 
-public class BlockReportSender {
+import java.io.File;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TimerTask;
 
+import com.dfs.nodes.DataNode;
+import com.dfs.utils.Connector;
+import com.dfs.utils.Constants;
+
+public class BlockReportSender extends TimerTask{
+	
+	List<String> blockIds = new ArrayList<>();
+	
+	@Override
+	public void run() {
+		Connector connector = new Connector();
+		Socket socket= connector.connectToNameNode(Constants.NAMENODE_BLOCK_PORT_NUM);
+		
+		try(ObjectOutputStream stream =  new ObjectOutputStream(socket.getOutputStream())){
+			blockIds.clear();
+			getBlockList(Constants.DATA_DIR);
+			BlockReport msg = new BlockReport(blockIds, DataNode.DATANODE_IP);
+			stream.writeObject(msg);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally{
+			connector.closeConnection(socket);
+		}
+	}
+
+	private void getBlockList(String directoryName) {
+		File directory = new File(directoryName);
+
+	    File[] filesList = directory.listFiles();
+	    if(filesList != null){
+		    for (File file : filesList) {
+		        if (file.isFile()) {
+		            String BlockPath = file.getParent();
+		            String blockId = BlockPath.substring
+		            		(BlockPath.lastIndexOf(File.separator)+1);
+		            blockIds.add(blockId);
+		        } 
+		        else
+		        	getBlockList(file.getAbsolutePath());
+		    }
+	    }
+	}
+	
 }
--- src/com/dfs/failure/HeartBeatSender.java
+++ src/com/dfs/failure/HeartBeatSender.java
@@ -1,5 +1,27 @@
 package com.dfs.failure;
 
-public class HeartBeatSender {
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.TimerTask;
 
+import com.dfs.messages.HeartBeatMessage;
+import com.dfs.utils.Connector;
+import com.dfs.utils.Constants;
+
+public class HeartBeatSender extends TimerTask{
+	@Override
+	public void run() {
+		Connector connector = new Connector();
+		Socket socket = connector.connectToNameNode(Constants.PORT_NUM);
+		
+		try(ObjectOutputStream stream =  new ObjectOutputStream(socket.getOutputStream())){
+			HeartBeatMessage msg = new HeartBeatMessage();
+			stream.writeObject(msg);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally{
+			connector.closeConnection(socket);
+		}
+	}
 }
